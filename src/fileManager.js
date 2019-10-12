aa7e4b15a2b4611de9fc78d17d9790c96f45700a
Hiếu Văn Trần <hieu.tran@enouvo.com>
2019-10-12 16:28:43 +0700

    update read json

---
+++ src/fileManager.js
@@ -0,0 +1,44 @@
+// const fs = require('fs');
+// module.exports = {
+//     createNewFile: (fileName) => {
+//         const fd = fs.openSync(fileName, 'w') // mo file roi moi sang lenh tiep theo dung openSync
+//     },
+//     // luu doi tuong vao file
+//     // stringify biến 1 object thành string
+//     saveJsonObjectToFile:(obj, fileName) => {
+//         const jsonString = JSON.stringify(obj)
+//         // ghi jsonString vao fileName
+//         fs.writeFile(fileName, jsonString, 'uft-8', (err, data) => {
+//             if(err) throw err; // nhay ra if error
+//             console.log(`Saved to file ${fileName}`);
+//         });
+//     }
+// };
+const fs = require('fs');
+module.exports = {
+    createNewFile: (fileName) => {
+        const fd = fs.openSync(fileName, 'w');//fd = file descriptor // mo file roi moi sang lenh tiep theo dung openSync
+    },
+    // luu doi tuong vao file
+    // stringify biến 1 object thành string
+    saveJsonObjectToFile: (obj, fileName) => {
+        const jsonString = JSON.stringify(obj);
+        fs.writeFile(fileName, jsonString, 'utf-8', (err, data) => {
+            if (err) throw err;
+            console.log(`Saved to file ${fileName}`);
+        });
+    },
+    // doc du lieu tu file ra
+    readJsonObjectFromFile: (fileName) => {
+        //Asynchronously reads the entire contents of a file
+        // readFile k dong bo 
+        fs.readFile(fileName, (err, data) => {
+            if (err) throw err;// nhay ra if error
+            let jsonObject = JSON.parse(data);
+            // console.log(`jsonObject.foods = ${JSON.stringify(jsonObject.foods)}`);
+            console.log(`jsonObject.resultCode = ${JSON.stringify(jsonObject.resultCode)}`);
+            console.log(`jsonObject.restaurantName = ${JSON.stringify(jsonObject.restaurantName)}`);
+            console.log(`jsonObject.address = ${JSON.stringify(jsonObject.address)}`);
+        });
+    }
+};
