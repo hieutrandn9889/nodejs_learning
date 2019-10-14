dc17f056b14b8ba990f2c2557f714e3800cd7b57
hieuTranEnouvo <hieu.tran@enouvo.com>
2019-10-14 19:48:51 +0700

    update router

--- myApp/routes/index.js
+++ myApp/routes/index.js
@@ -6,4 +6,20 @@ router.get('/', (req, res, next) => {
   res.render('index', { title: 'Hello everyone!' });
 });
 
+router.get('/list_all_foods', (req, res, next) => {
+  res.end("GET requested => list_all_foods")
+});
+
+router.post('/insert_new_food', (req, res, next) => {
+  res.end("POST requested => insert_new_food")
+});
+
+router.put('/update_a_food', (req, res, next) => {
+  res.end("PULL requested => update_a_food")
+});
+
+router.delete('/delete_a_food', (req, res, next) => {
+  res.end("DELETE requested => delete_a_food")
+});
+
 module.exports = router;
